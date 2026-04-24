1bc925f5fa3ecfe9d355f85dde38a75e151bbb21
itsbenchase <ben.chase@benchase.info>
2026-04-24 18:56:10 -0500

    kcm update??

--- gtfs2.java
+++ gtfs2.java
@@ -127,7 +127,7 @@ public class gtfs2
                     System.out.println("Calendar loaded");
                 }
 
-                else if (entry.getName().equals("calendar_dates.txt") && !found) {
+                else if (entry.getName().equals("calendar_dates.txt")) {
                     Scanner s = new Scanner(zis);
                     int z = 0;
                     int serviceIndex = -1, dateIndex = -1, exceptionIndex = -1;
